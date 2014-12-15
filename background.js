955edbfe58ada306bc44fe1f28d7e36329a6af49
Elliot <elliotforall@gmail.com>
2014-12-15 16:44:14 +0100

    Base de donnée pour toute la France
    L’extension permet désormais de chercher un livre dans toute la France
    grâce aux données de « Place des libraires ».

--- background.js
+++ background.js
@@ -2,8 +2,8 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     switch(request.type) {
         case "dom-loaded":
         	var productTitle = request.data.productTitle;
-        	chrome.browserAction.onClicked.addListener(function(tab) {    
-    		    var newURL = "http://www.parislibrairies.fr/dlivre.php?gencod="+productTitle;
+        	chrome.browserAction.onClicked.addListener(function(tab) { 
+				var newURL = "http://www.placedeslibraires.fr/dlivre.php?gencod="+productTitle+"&rid=";
     		    chrome.tabs.create({ url: newURL });
         	});
         	
